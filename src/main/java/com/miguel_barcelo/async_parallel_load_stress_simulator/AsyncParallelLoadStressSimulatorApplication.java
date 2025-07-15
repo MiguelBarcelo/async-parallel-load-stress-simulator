10aca8bd5cb775db2a0b4a730ef58000726c3788
Miguel Barcelo <miguel.barcelo@gmail.com>
2025-07-15 14:58:55 -0700

    feat: init project

---
+++ src/main/java/com/miguel_barcelo/async_parallel_load_stress_simulator/AsyncParallelLoadStressSimulatorApplication.java
@@ -0,0 +1,13 @@
+package com.miguel_barcelo.async_parallel_load_stress_simulator;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AsyncParallelLoadStressSimulatorApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(AsyncParallelLoadStressSimulatorApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/miguel_barcelo/async_parallel_load_stress_simulator/AsyncParallelLoadStressSimulatorApplicationTests.java
@@ -0,0 +1,13 @@
+package com.miguel_barcelo.async_parallel_load_stress_simulator;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AsyncParallelLoadStressSimulatorApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
