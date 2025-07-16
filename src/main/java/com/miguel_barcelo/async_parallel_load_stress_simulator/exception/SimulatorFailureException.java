cc35959c7b16739fefe1ffb370efa4cd08b25c0a
Miguel Barcelo <miguel.barcelo@gmail.com>
2025-07-15 20:17:51 -0700

    feat: add Exception

---
+++ src/main/java/com/miguel_barcelo/async_parallel_load_stress_simulator/exception/SimulatorFailureException.java
@@ -0,0 +1,7 @@
+package com.miguel_barcelo.async_parallel_load_stress_simulator.exception;
+
+public class SimulatorFailureException extends RuntimeException {
+	public SimulatorFailureException(String message) {
+		super(message);
+	}
+}
